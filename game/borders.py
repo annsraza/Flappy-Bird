a792dc9b68f60d6c521859a37fb3faa852be88fe
annsraza <annsraza.dev@gmail.com>
2026-07-05 23:05:03 +0500

    "feat: update borders file"

--- game/borders.py
+++ game/borders.py
@@ -1,16 +1,38 @@
 """
 borders.py
 
-Purpose:
-    Draw the game borders.
+Draws the border around the game area.
 """
 
 from game.constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
-class Border:
+class Borders:
 
     def __init__(self):
 
         self.width = SCREEN_WIDTH
         self.height = SCREEN_HEIGHT
+
+    def draw(self, renderer):
+
+        # Top
+        renderer.draw_text(
+            0,
+            0,
+            "+" + "-" * (self.width - 2) + "+"
+        )
+
+        # Bottom
+        renderer.draw_text(
+            0,
+            self.height - 1,
+            "+" + "-" * (self.width - 2) + "+"
+        )
+
+        # Left and Right
+        for y in range(1, self.height - 1):
+
+            renderer.draw_text(0, y, "|")
+
+            renderer.draw_text(self.width - 1, y, "|")
